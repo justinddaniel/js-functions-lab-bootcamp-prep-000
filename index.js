84ffe66cc3853ca1ac17ff079ac7645b05a7e547
Justin Daniel <justinddaniel@gmail.com>
2017-08-23 01:45:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-function happyHolidays(string) {
-  console.log('Happy holidays!')
+function happyHolidays() {
+  return 'Happy holidays!'
 }
 function HappyHolidaysTo($(name)) {
   return `Happy holidays, ${name}!`
